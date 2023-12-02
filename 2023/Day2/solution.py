b1373e3d028d58aa68284b8b2ef60cf3f25c08bf
n-garibli <118982611+n-garibli@users.noreply.github.com>
2023-12-02 14:07:57 +0000

    Day 2 solutions

---
+++ 2023/Day2/solution.py
@@ -0,0 +1,52 @@
+"""Advent of Code 2023 Day 2 Solution"""
+# Imports
+
+from typing import List, Dict
+import re
+
+# Load input
+with open("input.txt") as f:
+    games: List[str] = [line.strip() for line in f.readlines()]
+
+# Part 1 & 2
+# -----------------------------------------------------------------------------
+
+# Specifying the maximum number of each colour of cubes
+# as said in the problem for part 1
+max_cubes: Dict[str, int] = {"r": 12, "g": 13, "b": 14}
+
+# this will be updated as part of part 2
+game_powers = []
+
+# This is the sum of the ids for all possible games
+sum_game_ids: int = 0
+for game in games:
+    game_id: int = int(game.split(":")[0][5:])
+
+    # minimum number of cubes for this games to be possible
+    min_cubes: Dict[str, int] = {"r": 0, "g": 0, "b": 0}
+
+    # whether this game is impossible according to part 1 conditions
+    impossible: bool = False
+
+    # Extracting per round information
+    rounds: List[str] = game.split(":")[1].split(";")
+    for r in rounds:
+        for c in ["r", "b", "g"]:
+            matches = re.search(f"\d+ {c}", r)
+            if matches is not None:
+                n_cubes = int(matches.group()[:-1])
+                if n_cubes > min_cubes[c]:
+                    min_cubes[c] = n_cubes
+            else:
+                n_cubes = 0
+            if n_cubes > max_cubes[c]:
+                impossible = True
+    game_power = min_cubes["r"] * min_cubes["b"] * min_cubes["g"]
+    game_powers.append(game_power)
+
+    if not impossible:
+        sum_game_ids += game_id
+
+print(f"Part 1 Solution: {sum_game_ids}")
+print(f"Part 2 Solution: {sum(game_powers)}")
