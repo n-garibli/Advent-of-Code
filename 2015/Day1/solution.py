ba87721a3accb6d0d45d2f08d77dca1552ecfa4b
n-garibli <118982611+n-garibli@users.noreply.github.com>
2023-12-03 17:13:47 +0000

    added some flair and good practices

--- 2015/Day1/solution.py
+++ 2015/Day1/solution.py
@@ -1,15 +1,26 @@
 """Advent of Code 2015 Day 1 Solution"""
 
+from typing import Optional
+
 with open("input.txt") as f:
     parentheses = f.read()
 
-floor = 0
-basement_idx = None
+floor: int = 0
+
+# id of character when basement floor is first accessed
+basement_idx: Optional[int] = None  
 for i, bracket in enumerate(parentheses):
     if bracket == "(":
         floor += 1
     elif bracket == ")":
         floor -= 1
+    else:
+        raise (
+            ValueError(
+                f"Parentheses string can only contain brackets ( and ), got {bracket}"
+            )
+        )
+
     if floor == -1 and basement_idx is None:
         basement_idx = i + 1
 
