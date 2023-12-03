0365ea875166f66bf38e9a54c5b89217ffd7ff57
n-garibli <118982611+n-garibli@users.noreply.github.com>
2023-12-03 14:46:13 +0000

    add variable typing

--- 2023/Day3/solution.py
+++ 2023/Day3/solution.py
@@ -2,23 +2,24 @@
 
 # Imports
 import re
+from typing import List, Tuple
 
 # Load input line by line
 with open("input.txt") as f:
-    lines = [line.strip() for line in f.readlines()]
+    lines: List[str] = [line.strip() for line in f.readlines()]
 
-symbol_coors = []
+symbol_coors: List[Tuple[int, int, bool]] = []
 for i, line in enumerate(lines):
     for symbol in re.finditer(r"[^.^\d]", line):
         # store the coordinate of each symbol in the input
         # and specify whether it is a star
         symbol_coors.append((i, symbol.start(), symbol.group() == "*"))
 
-all_adj_nums = []  # tracks all numbers adjacent to a symbol
-gear_ratios = []
+all_adj_nums: List[int] = []  # tracks all numbers adjacent to a symbol
+gear_ratios: List[int] = []
 for symbol in symbol_coors:
     x, y, star = symbol
-    adj_nums = []
+    adj_nums: List[int] = []
     for line in lines[x - 1 : x + 2]:
         # looking for numbers in lines adjacent to symbol
         for num in re.finditer(r"\d+", line):
@@ -27,7 +28,7 @@ for symbol in symbol_coors:
 
     # These conditions determine whether the symbol is a valid gear
     if len(adj_nums) == 2 and star:
-        gear_ratio = adj_nums[0] * adj_nums[1]
+        gear_ratio: int = adj_nums[0] * adj_nums[1]
         gear_ratios.append(gear_ratio)
 
     all_adj_nums.extend(adj_nums)
