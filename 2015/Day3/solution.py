d8c5caf94da48ed7dcc31976c3c592f139ba0a93
n-garibli <118982611+n-garibli@users.noreply.github.com>
2023-12-04 14:37:51 +0000

    shorten code

--- 2015/Day3/solution.py
+++ 2015/Day3/solution.py
@@ -2,18 +2,17 @@
 # {Enter description for cell}
 # -----------------------------------------------------------------------------
 from typing import Dict, List, Tuple
-import numpy as np
 
 with open("input.txt") as f:
-    instructions: str = f.read()
+    instruct : str = f.read()
 
 # Mapping of the instruction symbols to a representation of
 # the direction of motion in a 2D plane
-directions: Dict[str, np.array] = {
-    "^": np.array([0, 1]),
-    ">": np.array([1, 0]),
-    "<": np.array([-1, 0]),
-    "v": np.array([0, -1]),
+dirs: Dict[str, Tuple[int,int]] = {
+    "^": (0, 1),
+    ">": (1, 0),
+    "<": (-1, 0),
+    "v": (0, -1),
 }
 
 
@@ -22,20 +21,14 @@ def get_coors(instructions: str) -> List[Tuple[int, int]]:
     instructions provided"""
 
     coors_visited: List[Tuple[int, int]] = []
-
-    # Using numpy arrays to store coordinates as they are mutable and
-    # support easy elementwise addition
-    coor: np.array = np.array([0, 0])
+    coor: Tuple[int,int] = (0, 0)
+    
     for i in instructions:
-        if tuple(coor) not in coors_visited:
-            coors_visited.append(tuple(coor))
-        coor += directions[i]
+        if coor not in coors_visited:
+            coors_visited.append(coor)
+        coor = (coor[0] + dirs[i][0], coor[1] + dirs[i][1])
     return coors_visited
 
 
-# Extracting instructions meant for santa and for robo santa for part 2
-for_santa: str = instructions[0::2]
-for_robo_santa: str = instructions[1::2]
-
-print(f"Part 1 Solution: {len(get_coors(instructions))}")
-print(f"Part 2 Solution: {len(set(get_coors(for_santa) + get_coors(for_robo_santa)))}")
+print(f"Part 1 Solution: {len(get_coors(instruct))}")
+print(f"Part 2 Solution: {len(set(get_coors(instruct[::2]) + get_coors(instruct[1::2])))}")
