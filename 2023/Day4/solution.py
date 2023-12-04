2f686f62f4e5f24f3ed4949379a7c293e0f37f2e
n-garibli <118982611+n-garibli@users.noreply.github.com>
2023-12-04 10:52:37 +0000

    day 4 2023 solution

---
+++ 2023/Day4/solution.py
@@ -0,0 +1,37 @@
+"""Advent of Code 2023 Day 4 Solution"""
+
+from typing import List, Dict
+from collections import defaultdict
+import re
+
+with open("input.txt") as f:
+    # parsing the input line by lines
+    cards: List[str] = [line.strip() for line in f.readlines()]
+
+total_points: int = 0
+
+# stores the number of card copies for each card id with the default being 1
+n_cards: Dict[int, int] = defaultdict(lambda: 1)
+max_card_id: int = len(cards)
+
+for i, card in enumerate(cards):
+    card_id = i + 1
+    # use regex to find all digits
+    winning_numbers = re.findall(r"\d+", card.split(":")[1].split("|")[0])
+    our_numbers = re.findall(r"\d+", card.split(":")[1].split("|")[1])
+    n_matches = len([x for x in our_numbers if x in winning_numbers])
+
+    # This solves the part 1 problem
+    card_points = 2 ** (n_matches - 1) if n_matches > 0 else 0
+    total_points += card_points
+
+    # This solves the part 2 problem
+    for j in range(1, n_matches + 1):
+        if card_id + j > max_card_id:
+            # no need to compute copies of cards that dont exist
+            break
+        # creates a copy of the card_id+jth card for each copy of current card
+        n_cards[card_id + j] += 1 * n_cards[card_id]
+
+print(f"Part 1 Solution: {total_points}")
+print(f"Part 2 Solution: {sum(n_cards.values())}")
