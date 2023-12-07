620101511f43f74065e0c6197020fd1cda7bf628
n-garibli <118982611+n-garibli@users.noreply.github.com>
2023-12-07 17:10:04 +0000

    day 6

---
+++ 2022/Day6/solution.py
@@ -0,0 +1,16 @@
+"""Advent of Code 2022 Day 6 Solution"""
+
+with open("input.txt") as f:
+    signal: str = f.read()
+
+
+def marker_id(signal: str, n_unique: int):
+    """Returns the first index where the preceding n_unique elements
+    of signal are different from eachother"""
+    for i in range(n_unique, len(signal)):
+        if len(set(signal[i - n_unique : i])) == n_unique:
+            return i
+
+
+print(f"Part 1 Solution: {marker_id(signal, 4)}")
+print(f"Part 2 Solution: {marker_id(signal, 14)}")
