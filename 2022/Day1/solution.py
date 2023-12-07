0cc6344e8103627176f221e76f306308f835ba31
n-garibli <118982611+n-garibli@users.noreply.github.com>
2023-12-07 09:05:00 +0000

    reindeers not elves

--- 2022/Day1/solution.py
+++ 2022/Day1/solution.py
@@ -3,13 +3,13 @@
 from typing import List
 
 with open("input.txt") as f:
-    # Parsing input to extract a list of inventories (one per elf)
+    # Parsing input to extract a list of inventories (one per reindeer)
     inventories: List[List[int]] = [
         list(map(int, x.splitlines())) for x in f.read().split("\n\n")
     ]
 
-cals_per_elf: List[int] = [sum(i) for i in inventories]
+cals_per_reindeer: List[int] = [sum(i) for i in inventories]
 print(f"Part 1 Solution: {max(cals_per_elf)}")
 
-cals_per_elf.sort()
+cals_per_reindeer.sort()
 print(f"Part 2 Solution: {sum(cals_per_elf[-3:])}")
