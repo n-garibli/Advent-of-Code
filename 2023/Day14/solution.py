4316161e3939feb0ed5e087abec2f3c6763c3bf4
n-garibli <118982611+n-garibli@users.noreply.github.com>
2023-12-16 11:31:40 +0000

    refactor and add more comments for clarity

--- 2023/Day14/solution.py
+++ 2023/Day14/solution.py
@@ -17,61 +17,65 @@ for i in range(d):
 original_rock_grid: np.ndarray = copy.copy(rock_grid)
 
 
-def tilt(rock_grid: np.ndarray, tilt: str) -> np.array:
-    """Mutates input array as if performing tilt"""
-    if tilt == "w":
-        rock_grid = rock_grid.transpose()
-    elif tilt == "e":
-        rock_grid = np.flip(rock_grid.transpose(), axis=0)
-    elif tilt == "s":
-        rock_grid = np.flip(rock_grid, axis=0)
-    else:
-        assert tilt == "n", f"Got invalid direction {tilt}"
+def tilt_north(rock_grid: np.array) -> np.array:
+    """Alters the input array as if performing a tilt to the north, so that
+    all the round rocks (O) go to the top"""
 
     for i in range(d):
+        # This is a list of Os in column i split by the # between them
+        # Eg: ["","OOOO",""] implies that between the first 2 # there are 4 Os
         O: List[str] = "".join(rock_grid[:, i][rock_grid[:, i] != "."]).split("#")
-        # remove all Os from collumn so that they can be put in the right
-        # place instead
+        # remove all Os from column i (to be put in a new place later)
         rock_grid[:, i][rock_grid[:, i] == "O"] = "."
         if "O" in O[0]:
+            # means there were Os before the first #, putting them all at the top
             rock_grid[:, i][: len(O[0])] = "O"
 
         for j, k in enumerate(np.where(rock_grid[:, i] == "#")[0]):
+            # putting all the Os that come after a particular # right after it
             if O[j + 1] != "":
                 rock_grid[:, i][k + 1 : k + 1 + len(O[j + 1])] = "O"
 
-    # flip array back to normal if not going north
-    if tilt == "w":
-        rock_grid = rock_grid.transpose()
-    elif tilt == "e":
-        rock_grid = np.flip(rock_grid, axis=0).transpose()
-    elif tilt == "s":
-        rock_grid = np.flip(rock_grid, axis=0)
     return rock_grid
 
 
-def get_north_load(grid: np.ndarray) -> int:
+def get_north_load(grid: np.array) -> int:
     """Computes north load given arrangement of rocks"""
     return sum([np.sum(grid[i] == "O") * (d - i) for i in range(d)])
 
 
-print(f"Part 1 Solution: {get_north_load((tilt(rock_grid,'n')))}")
+def cycle(rock_grid: np.array) -> np.array:
+    """This function completes one cycle of tilting the rocks in every
+    direction"""
+    for _ in range(4):
+        rock_grid = np.rot90(tilt_north(rock_grid), k=-1)
+    return rock_grid
 
-rock_grid = copy.copy(original_rock_grid)
 
+print(f"Part 1 Solution: {get_north_load((tilt_north(rock_grid)))}")
+
+# rock_grid has mutated whilst solving part 1 so resetting it
+rock_grid = original_rock_grid
+
+# using the tobytes() method to make a np array hashable
+cache = [rock_grid.tobytes()]  # stores previously seen rock arrangements
+# stores north loads for all previously seen rock arrangements
 cache_vals = {rock_grid.tobytes(): get_north_load(rock_grid)}
-cache = [rock_grid.tobytes()]
 
+
+# This loop search for an inevitable cycle in the rock movements
+# by checking if an arrangement of rocks has previously been seen
 while True:
-    for direction in "nwse":
-        rock_grid = tilt(rock_grid, direction)
+    rock_grid = cycle(rock_grid)
     h = rock_grid.tobytes()
     cache_vals[h] = get_north_load(rock_grid)
     if h in cache:
         start = cache.index(h)
-        cycle = len(cache) - start
+        # calculating the length of the cycle
+        cycle_l = len(cache) - start
         break
     else:
         cache.append(h)
 
-print(f"Part 2 Solution: {cache_vals[cache[(1000000000 - start) % cycle + start]]}")
+# The arrangement after 1000000000 cycles will be the same as after (1000000000 - start) % cycle_l + start based on the cycle length and where the cycle begins
+print(f"Part 2 Solution: {cache_vals[cache[(1000000000 - start) % cycle_l + start]]}")
