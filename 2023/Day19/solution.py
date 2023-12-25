f1ef2238084c00144642814f8c440ba908d5217e
n-garibli <ngaribli@gmail.com>
2023-12-25 13:22:03 +0500

    2023 day 19 full solution

--- 2023/Day19/solution.py
+++ 2023/Day19/solution.py
@@ -1,20 +1,22 @@
 """Advent of Code 2023 Day 19 Solution
-Completion date: 24/12/2023"""
+Completion date: 25/12/2023"""
 
-from typing import List, Dict
+from typing import List, Dict, Tuple
+import copy
+from math import prod
 
 with open("input_test.txt") as f:
-    full_input = f.read().splitlines()
-    split_i = full_input.index("")
+    inp = f.read().splitlines()
+    split_i = inp.index("")
 
 # Store all the rules as a dictionary with their names as keys
-rules: Dict[str, str] = {
-    name: rule[:-1] for name, rule in [r.split("{") for r in full_input[:split_i]]
-}
+# E.g. : {'in': 's<1351:px,qqz'}
+rules: Dict[str, str] = {n: r[:-1] for n, r in [r.split("{") for r in inp[:split_i]]}
 
 # Store the part infos as a list of dictionaries
+# E.g. : [{'x' : 1, 'm' : 2, 'a' : 3, 's': 4},....]
 parts: List[Dict[str, int]] = []
-for p in full_input[split_i + 1 :]:
+for p in inp[split_i + 1 :]:
     scores: Dict[str, int] = {
         s1: int(s2) for s1, s2 in [s.split("=") for s in p[1:-1].split(",")]
     }
@@ -22,7 +24,7 @@ for p in full_input[split_i + 1 :]:
 
 
 def part_accepted(part: Dict[str, int]) -> bool:
-    """Determines whether a part is accepted given its scores"""
+    """Determines whether a part is accepted given its scores."""
     outcome = check_rule(part, "in")
     while outcome not in ["R", "A"]:
         outcome = check_rule(part, outcome)
@@ -46,4 +48,54 @@ def check_rule(part: Dict[str, int], rule_name: str) -> str:
     return conditions[-1]
 
 
-print(f"Part 1 Solution: {sum([sum(p.values()) for p in parts if part_accepted(p)])}")
+print(f"Part 1 Solution: {sum(sum(p.values()) for p in parts if part_accepted(p))}")
+
+# stores dictionaries containing accepted combinations of ranges of values for each variable
+accepted_combos: List[Dict[str, Tuple[int, int]]] = []
+
+
+def find_accepted_combos(
+    rules: Dict[str, str],  # See how rules is defined above
+    ranges: Dict[str, Tuple[int, int]],  # See example below
+    start_rule_name: str = "in",
+) -> None:
+    """Checks all the rules in the predefined rules dictionary and populates
+    the predefind accepted_combos list. Every variable (x,m,a,s) is assumed to
+    only be allowed to take values in a range defined by the ranges dictionary."""
+    conditions = rules[start_rule_name].split(",")
+    otherwise = conditions[-1]  # result if all conditions fail
+    for c in conditions[:-1]:
+        cond, result = c.split(":")
+        var = cond[0]  # scoring varible to test (x,m,a,s)
+        val = int(cond[2:])  # value that var must be bigger/smaller than
+
+        if cond[1] == ">":
+            pass_range = tuple([val + 1, ranges[var][1]])
+            fail_range = tuple([ranges[var][0], val + 1])
+        else:
+            pass_range = tuple([ranges[var][0], val])
+            fail_range = tuple([val, ranges[var][1]])
+
+        ranges_pass = copy.copy(ranges)
+        ranges_pass[var] = pass_range
+
+        if result == "A":
+            accepted_combos.append(ranges_pass)
+        elif result != "R":
+            find_accepted_combos(rules, ranges_pass, result)
+
+        # continue checking other conditions given this one failed
+        ranges[var] = fail_range
+
+    # At this point the ranges dict has been modified to contain only the failed ranges
+    if otherwise == "A":
+        accepted_combos.append(ranges)
+    elif otherwise != "R":
+        find_accepted_combos(rules, ranges, otherwise)
+
+
+ranges = {"x": (1, 4001), "m": (1, 4001), "a": (1, 4001), "s": (1, 4001)}
+find_accepted_combos(rules, ranges)
+# Ranges in accepted_combos will be non overlapping hence can sum the products of all the 
+# lengths of the possible ranges for each variable
+print(f"Part 2 Solution: {sum(prod(x[1] - x[0] for x in rs.values()) for rs in accepted_combos)}")
