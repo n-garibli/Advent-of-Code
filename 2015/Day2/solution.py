7d1b4470753510ae0166f3f85605585399e6fccb
n-garibli <ngaribli@gmail.com>
2023-12-25 15:34:25 +0500

    minor reformat

--- 2015/Day2/solution.py
+++ 2015/Day2/solution.py
@@ -1,21 +1,20 @@
-"""Advent of Code 2015 Day 2 Solution"""
+"""Advent of Code 2015 Day 2 Solution
+Completion Date: 25/12/2013"""
 
 from typing import List
 
 with open("input_test.txt") as f:
     # load and parse the input
-    present_dims: List[List[str]] = [line.strip().split("x") for line in f.readlines()]
+    p_dims: List[List[int]] = [map(int, i.split("x")) for i in f.read().splitlines()]
 
 total_paper: int = 0
 total_ribbon: int = 0
-for dims in present_dims:
-    l, w, h = [int(d) for d in dims]
+for dims in p_dims:
+    l, w, h = dims
     side_areas: List[int] = [l * w, w * h, l * h]
-    paper_needed = min(side_areas) + 2 * sum(side_areas)
-    ribbon_to_wrap = min([2 * l + 2 * w, 2 * w + 2 * h, 2 * h + 2 * l])
-    ribbon_for_bow = l * w * h
-    total_paper += paper_needed
-    total_ribbon += ribbon_to_wrap + ribbon_for_bow
+    total_paper += min(side_areas) + 2 * sum(side_areas)
+    ribbon_to_wrap: int = min([2 * l + 2 * w, 2 * w + 2 * h, 2 * h + 2 * l])
+    total_ribbon += ribbon_to_wrap + l * w * h  # add ribbon for bow too
 
 print(f"Part 1 Solution: {total_paper}")
 print(f"Part 2 Solution: {total_ribbon}")
