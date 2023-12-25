a6f395cf8abf0f5af1bc1fd9a4284845e0e271c2
n-garibli <ngaribli@gmail.com>
2023-12-25 15:39:55 +0500

    add completion date

--- 2015/Day4/solution.py
+++ 2015/Day4/solution.py
@@ -1,21 +1,22 @@
-"""Advent of Code 2015 Day 4 Solution"""
+"""Advent of Code 2015 Day 4 Solution
+Competion Date: 04/12/2013"""
 
 import hashlib as h
 
-my_input = "abcdef"
+input_ = "abcdef"
 
 digit: int = 0
-hash_pt1: int = 0
-hash_pt2: int = 0
+hash1: int = 0
+hash2: int = 0
 
-while not (hash_pt1 and hash_pt2):
-    key: str = my_input + str(digit)
+while not (hash1 and hash2):
+    key: str = input_ + str(digit)
     key_hash: str = h.md5(key.encode()).hexdigest()
-    if key_hash[:5] == "00000" and not hash_pt1:
-        hash_pt1 = digit
-    if key_hash[:6] == "000000" and not hash_pt2:
-        hash_pt2 = digit
+    if key_hash[:5] == "00000" and not hash1:
+        hash1 = digit
+    if key_hash[:6] == "000000" and not hash2:
+        hash2 = digit
     digit += 1
 
-print(f"Part 1 Solution: {hash_pt1}")
-print(f"Part 2 Solution: {hash_pt2}")
+print(f"Part 1 Solution: {hash1}")
+print(f"Part 2 Solution: {hash2}")
